89c4384954d85a42d05d734a22ad7d3cda0f1112
Kim6-9 <you@example.com>
2024-06-06 01:36:11 -0700

    my commit

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,31 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node -  will create a new B-T node
- *
- * @parent: - a PTR to parent of created node.
- * @value: - content of my new born node
- * Return: PTR > new node or NULL > failure.
+ * binary_tree_node - Create a binary node
+ * @parent: node parent
+ * @value: Value of the node
+ * Return: the new node
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	/* BT Node  declation + DT */
-	binary_tree_t *n_node;
+	binary_tree_t *new_node;
 
-	/* Memory allocation for n_node var*/
-	n_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-
-	if (n_node == NULL)
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-
-	/* values assigment */
-	n_node->parent = parent;
-	n_node->n = value;
-	n_node->left = NULL;
-	n_node->right = NULL;
-
-	/* n_node's PTR to be returned */
-	return (n_node);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
 }
